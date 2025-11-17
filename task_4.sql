19ab8bb63ac6242bee970a49c27f71de604239f9
Amr Khaled <amrkh3bkarino10@gmail.com>
2025-11-18 01:21:25 +0200

    dd task_4.sql: print full description of books table without DESCRIBE/EXPLAIN

---
+++ task_4.sql
@@ -0,0 +1,16 @@
+-- task_4.sql
+USE alx_book_store;
+-- Print full description of the table 'books' without using DESCRIBE or EXPLAIN
+
+SELECT 
+    COLUMN_NAME AS `Column`,
+    COLUMN_TYPE AS `Type`,
+    IS_NULLABLE AS `Nullable`,
+    COLUMN_KEY AS `Key`,
+    COLUMN_DEFAULT AS `Default`,
+    EXTRA AS `Extra`
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store'  -- اسم قاعدة البيانات
+    AND TABLE_NAME = 'books';
